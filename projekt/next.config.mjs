4300c9073b00de3ad12964c67863c0f6d1616809
Heva Khalaf <heva319@gmail.com>
2025-12-04 15:19:50 +0100

    Allow Render image domain

--- projekt/next.config.mjs
+++ projekt/next.config.mjs
@@ -4,8 +4,10 @@ const nextConfig = {
     // http://localhost:4000/file-bucket/
     remotePatterns: [
       { protocol: 'http', hostname: 'localhost', port: '4000', pathname: '/**' },
-  
       { protocol: 'http', hostname: '127.0.0.1', port: '4000', pathname: '/**' },
+
+      // ✅ إضافة دومين Render
+      { protocol: 'https', hostname: 'swaphub-api-2kbm.onrender.com', pathname: '/**' },
     ],
   },
 };
